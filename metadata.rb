87f6923fadf9c24f569147a6c4612e05170dd559
Mihai Petracovici <m-petracovici@wiu.edu>
2015-09-10 17:43:30 -0500

    Use new apache2 cookbook

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,12 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Installs passenger for Apache2'
-version           '2.2.3'
+version           '2.2.4'
 
 recipe 'passenger_apache2', 'Installs Passenger as an Apache module'
 recipe 'passenger_apache2::mod_rails', 'Enables Apache module configuration for passenger module'
 
-depends 'apache2', '~> 1.0'
+depends 'apache2', '~> 3.1.0'
 depends 'build-essential'
 
 %w( redhat centos scientific amazon oracle ubuntu debian arch ).each do |os|
